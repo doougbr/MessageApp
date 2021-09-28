2a34c81eb8c5b1687a67de12a34d90756db1f1fb
Douglas Bittencourt Ribeiro <dougbr505@gmail.com>
2021-09-28 20:25:29 -0300

    navigation added

--- app/src/main/java/com/example/messageapp/MainActivity.kt
+++ app/src/main/java/com/example/messageapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.messageapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.messageapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+private var binding: ActivityMainBinding? = null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding?.root
+        setContentView(view)
+
+        val bottomNav = binding!!.bottomNavigationView
+        val navController = findNavController(R.id.fragment)
+
+        bottomNav.setupWithNavController(navController)
+
+
+
     }
 }
--- app/src/main/java/com/example/messageapp/MessagesList.kt
+++ app/src/main/java/com/example/messageapp/MessagesList.kt
@@ -1,5 +1,48 @@
 package com.example.messageapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.example.messageapp.databinding.FragmentMessagesListBinding
 
-class MessagesList : Fragment(R.layout.fragment_messages_list)
+class MessagesList : Fragment(R.layout.fragment_messages_list) {
+
+    private var _binding : FragmentMessagesListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMessagesListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+/*
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val messagesList = arrayListOf(
+            "Oi",
+            "Bom dia",
+            "Boa tarde",
+            "Boa noite",
+            "Tchau")
+        val listView = binding.listViewMessages
+        val adapter = activity?.let { ArrayAdapter(it.baseContext, android.R.layout.simple_list_item_1, messagesList) }
+        listView.adapter = adapter
+        adapter?.notifyDataSetChanged()
+    }
+*/
+
+    // to avoid memory leaks
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
